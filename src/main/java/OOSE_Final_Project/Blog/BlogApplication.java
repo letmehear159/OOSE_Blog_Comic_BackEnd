3b7f7b80010acdeb43d1e2d6ad419c25177e7a85
Nguyễn Trường <nguyentruongpro19@gmail.com>
2025-05-12 20:42:58 +0700

    Change reading env variable to main

--- src/main/java/OOSE_Final_Project/Blog/BlogApplication.java
+++ src/main/java/OOSE_Final_Project/Blog/BlogApplication.java
@@ -1,5 +1,6 @@
 package OOSE_Final_Project.Blog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,8 +12,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 public class BlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()
+                              .load();
 
+        setIfPresent("MAIL_USERNAME", dotenv);
+        setIfPresent("MAIL_PASSWORD", dotenv);
+        setIfPresent("FRONT_END_URL", dotenv);
+        setIfPresent("SECRET_KEY", dotenv);
+        setIfPresent("GOOGLE_CLIENT_SECRET", dotenv);
+        setIfPresent("BACK_END_URL", dotenv);
+        setIfPresent("DATA_SOURCE", dotenv);
+        setIfPresent("DB_USERNAME", dotenv);
+        setIfPresent("DB_PASSWORD", dotenv);
+
+
+        SpringApplication.run(BlogApplication.class, args);
+    }
+
+    private static void setIfPresent(String key, Dotenv dotenv) {
+        String value = dotenv.get(key);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
+    }
 }
