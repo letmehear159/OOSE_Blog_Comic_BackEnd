89a3ed58e3274ac37182cee3fa0f4a09d316d710
“myuh250” <huyng2724@gmail.com>
2025-05-13 16:45:45 +0700

    Update for upload avatar usecase

--- src/main/java/OOSE_Final_Project/Blog/config/WebConfig.java
+++ src/main/java/OOSE_Final_Project/Blog/config/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(url)
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
